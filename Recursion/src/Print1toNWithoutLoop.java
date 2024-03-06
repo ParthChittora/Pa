48b0500e95eaea038f41df5ccf6152bfb124d5eb
Parth Chittora <chittora14parth@gmail.com>
2024-03-06 16:46:53 +0530

    Print 1 to N using recursion added

--- Recursion/src/Print1toNWithoutLoop.java
+++ Recursion/src/Print1toNWithoutLoop.java
@@ -1,5 +1,12 @@
-import java.util.Arrays;
-
 public class Print1toNWithoutLoop {
-kdjfj
+    public static void print1ToN(int n){
+        if(n==0)
+            return;
+        print1ToN(n-1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        print1ToN(100);
+    }
 }
