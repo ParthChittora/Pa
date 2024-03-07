f0533010004ea3ea4aaa12674104844b99a9b0a6
Parth Chittora <chittora14parth@gmail.com>
2024-03-07 22:02:59 +0530

    spaces deleted

--- Recursion/src/CheckingPrimeUsingRecursion.java
+++ Recursion/src/CheckingPrimeUsingRecursion.java
@@ -1,19 +1,13 @@
 public class CheckingPrimeUsingRecursion {
     public static boolean isPrime(int n, int i) {
-
-        // Base cases
         if (n <= 2)
             return n == 2;
         if (n % i == 0)
             return false;
         if (i * i > n)
             return true;
-
-        // Check for next divisor
         return isPrime(n, i + 1);
     }
-
-    // Driver program to test above function
     public static void main(String[] args) {
         int n = 15;
         if (isPrime(n, 2))
